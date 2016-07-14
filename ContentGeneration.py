d973ea141b93ef4c603a2d62298935566905aa1f
Charles Reid <charlesreid1@gmail.com>
2016-07-14 10:59:59 -0700

    content is now asteroids and dinosaurs.

--- ContentGeneration.py
+++ ContentGeneration.py
@@ -1,45 +1,68 @@
 import json
-from olipy.queneau import WordAssembler
+from olipy.queneau import Assembler, WordAssembler
 from olipy.data import load_json
 import textwrap
 
 prefix = "/Volumes/noospace/Users/charles/codes/asmodeus/"
 
 
-
-def generate_content(topic='dinosaurs'):
+def generate_content(nitems=1):
     """
-    This method generates some text.
+    This method generates some text about
+    dinosaurs and asteroids.
 
-    The default topic is dinosaurs. 
-
-    It could use some more arguments:
-    - Dinosaurs, or asteroids?
-    - Custom text generation modules?
-    - Number of lines/items?
+    - Number of lines/items = 1
     """
 
-
-
+    body = ""
 
     # =================================
-    # Assemble Words
+    # Asteroids
+
+    body += "Asteroid of the Day: "
+
+    corpus = Assembler.load(open("olipy/data/minor_planets.min.json"), tokens_in='citation')
+
+    sentences = []
+    names = []
+    for sentence, source in corpus.assemble("f.l", min_length=4):
+        sentences.append(sentence)
+        names.append(source['name'])
+
+    # Make a new assembler from the names of the asteroids that were chosen.
+    name_assembler = WordAssembler(names)
+    name = name_assembler.assemble_word()
+
+    asteroid = name
+    asteroid += "\n"*2
+
+    for s in " ".join(sentences):
+        asteroid += s
+
+    body += asteroid 
+    body += "\n"*2
+
+    # =================================
+    # Dinosaurs
+
+    body += "In an alternate universe, this asteroid would have annihilated the following dinosaur species:"
+    body += "\n"*2
 
     assembler = WordAssembler(load_json("dinosaurs.json"))
     
-    body = ""
-
     dinos = []
-    for z in range(10):
+    for z in range(20):
 
         dino = assembler.assemble_word()
         if dino[0] in 'AEIO':
-            dino = "My favorite dinosaur is an " + dino
+            dino = "* " + dino
         else:
-            dino = "My favorite dinosaur is a " + dino
+            dino = "* " + dino
         dinos.append(dino)
 
-    body = "\n\n".join(dinos)
+    body += "\n".join(dinos)
+
+    # ===============================
     
     return body
 
